6dc06550ef87072bfc26c5f851cb1c1b21d433c6
Peter Lieven <pl@kamp.de>
2012-11-29 13:23:33 +0100

    PDU queue out PDUs in order of itt only if the outqueue is at risk to block
    Previous commit 1f4a66a sorted all PDUs in order of their itt. In fact
    the only command that could be blocked would have been a Data-Out PDU
    (or a retransmitted PDU). Sorting all PDUs according to itt lead to
    all Data-OUT PDUs being queued before consecutive Command PDUs.
    This turned out to be a performance drawback.
    
    We switched the behaviour to only queue PDUs in front places of the queue
    if there is an PDU in the queue that would block the outqueue (leading
    to a deadlock).
    
    Signed-off-by: Peter Lieven <pl@kamp.de>

--- lib/socket.c
+++ lib/socket.c
@@ -79,7 +79,8 @@ iscsi_add_to_outqueue(struct iscsi_context *iscsi, struct iscsi_pdu *pdu)
 	 * queue pdus with itt = 0xffffffff (SNACK / DataACK) in order but at head of queue.
 	 */
 	do {
-		if (current->written == 0 && (iscsi_serial32_compare(pdu->itt, current->itt) < 0 
+		if (current->written == 0 && 
+		((iscsi_pdu_blocks_outqueue(iscsi, current) && iscsi_serial32_compare(pdu->itt, current->itt) < 0)
 			|| (pdu->itt == 0xffffffff && current->itt != 0xffffffff))) {
 			/* insert PDU before the current */
 			if (last != NULL) {
